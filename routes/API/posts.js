9074edbb1048b734993e4f898aa87a6711eab50d
Timmy Cho <chotimmysf@gmail.com>
2021-10-30 00:45:47 -0700

    like and unlike a post

--- routes/API/posts.js
+++ routes/API/posts.js
@@ -46,4 +46,69 @@ async (req,res) => {
 }
 );
 
+// @route GET API/posts
+// @desc Get all posts
+// @access Private
+Router.get('/', auth, async(req, res) => {
+    try {
+        // Sort posts by newest first
+        const posts = await Post.find().sort({date: -1});
+    } catch(err) {
+    console.error(err.message);
+    res.status(500).send('Server error.');
+}
+});
+
+// @route DELETE API/posts/:id
+// @desc Delete a post
+// @access Private
+Router.delete('/:id', auth, async(req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        if (!post) {
+            return res.status(404).json({msg: 'Post not found.'});
+        }
+
+        if (!user) {
+            return res.status(404).json({msg: 'User not found.'});
+        }
+
+        // Check user
+        if (post.user.toString() !== req.user.id) {
+            return res.status(401).json({msg: 'User not authorized'});
+        }
+
+        await post.remove();
+
+        res.json({msg: 'Post removed successfully.'});
+    } catch(err) {
+    console.error(err.message);
+    res.status(500).send('Server error.');
+}
+});
+
+// @route PUT API/posts/like/:id
+// @desc Like a post
+// @access Private
+Router.put('/like/:id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // Check if post has already been liked
+        if(post.likes.filter(like => like.user.toString() === req.user.id).length > 0) {
+            return res.json(400).json('You already liked this post.');
+        }
+
+        post.likes.unshift({ user: req.user.id });
+
+        await post.save();
+
+        res.json(post.likes);
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server error.');
+    }
+});
+
 module.exports = Router;
