e12a5f03833be146c3f40e30f0e28908e8cab5a5
Timmy Cho <chotimmysf@gmail.com>
2021-11-05 19:01:11 -0700

    add and remove comments

--- routes/API/posts.js
+++ routes/API/posts.js
@@ -100,11 +100,82 @@ Router.put('/like/:id', auth, async (req, res) => {
             return res.json(400).json('You already liked this post.');
         }
 
-        post.likes.unshift({ user: req.user.id });
+        // Get remove index
+        const removeIndex = post.likes.map(like => like.user.toString()).indexOf(req.user.id);
+
+        post.likes.splice(removeIndex, 1);
 
         await post.save();
 
         res.json(post.likes);
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server error.');
+    }
+});
+
+// @route GET API/posts/comment/:id
+// @desc Comment on a post
+// @access Private
+Router.post('/comment/:id', [
+    auth, [
+        check('text',"Text is required.").not().isEmpty()
+    ]
+],
+async (req,res) => {
+    const errors = validationResult(req);
+    if(!errors.isEmpty()) {
+        return res.status(400).json({errors: errors.array() });
+    }
+
+    try {
+        // Bring in and await for User model
+        const user = await User.findById(req.user.id).select('-password');
+        const post = await Post.findById(req.params.id);
+
+        const newComment = {
+            text: req.body,
+            name: user.name,
+            avatar: user.avatar,
+            user: req.user.id
+        };
+
+        post.comments.unshift(newComment);
+
+        res.json(post.comments);
+    } catch(err) {
+        console.error(err.message);
+        res.status(500).send('Server error.')
+    }
+
+}
+);
+
+// @route PUT API/posts/comment/:id/:comment_id
+// @desc Remove a comment
+// @access Private
+Router.delete('/comment/:id/:comment_id', auth, async (req, res) => {
+    try {
+        const post = await Post.findById(req.params.id);
+
+        // Post comment
+        const comment = post.comments.find(comment => comment.id === req.params.comment_id);
+
+        // Ensure comment exists
+        if(!comment) {
+            return res.status(404).json({msg: 'Comment does not exist.'});
+        }
+
+        // Check if OP is initiating the deletion
+        if(comment.user.toString() !== req.user.id) {
+            return res.status(401).json({msg: 'You cannot delete the comment since you did not write it.'});
+        }
+
+        // Get remove index
+        const removeIndex = post.comments
+            .map(comment => comment.user.toString())
+            .indexOf(req.user.id);
+
     } catch(err) {
         console.error(err.message);
         res.status(500).send('Server error.');
