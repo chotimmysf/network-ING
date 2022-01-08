0590ac80b15d72a0373efa0e9711c9811e8ac232
Timmy Cho <chotimmysf@gmail.com>
2022-01-07 22:50:36 -0800

    dashboard route

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,14 @@
 import React, { Fragment, useEffect } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+// Components of Web App
 import Navbar from './components/layout/navbar';
 import Landing from './components/layout/landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login'; 
 import Alert from './components/layout/Alert';
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute'
 
 // Redux
 import { Provider } from 'react-redux';
@@ -29,6 +33,7 @@ const App = () => {
           <Switch>
             <Route exact path = "/register" component={Register}/>
             <Route exact path = "/login" component={Login}/>
+            <PrivateRoute exact path = "/dashboard" component={Dashboard}/>
           </Switch>
         </section>
       </Fragment>
