b315f41e2e916ae9d7380ded85b4e378789e93dd
Timmy Cho <chotimmysf@gmail.com>
2022-02-21 21:36:19 -0800

    displaying profile experience and education

--- client/src/components/profile/Profile.js
+++ client/src/components/profile/Profile.js
@@ -5,8 +5,9 @@ import { connect } from 'mongoose';
 import Spinner from '../layout/Spinner';
 import ProfileTop from './ProfileTop';
 import ProfileAbout from './ProfileAbout';
+import ProfileExperience from './ProfileExperience';
+import ProfileEducation from './ProfileEducation';
 import { getProfileById } from '../../actions/profile';
-import ProfileAbout from './ProfileAbout';
 
 const Profile = ( { getProfileById, profile: { profile, loading}, auth, match}) => {
   useEffect(() => {
@@ -22,6 +23,30 @@ const Profile = ( { getProfileById, profile: { profile, loading}, auth, match}) 
             <div class="profile-grid my-1">
                 <ProfileTop profile={profile} />
                 <ProfileAbout profile={profile} />
+                <div className="profile-exp bg-white p-2">
+                    <h2 className="text-primary">Experience</h2>
+                </div>
+                {profile.experience.length > 0 ? (
+                    <Fragment>
+                        {profile.experience.map(experience => (
+                            <ProfileExperience key={experience._id} experience={experience}/>
+                        ))}
+                    </Fragment>
+                ): (
+                    <h3>No experience credentials</h3>
+                )}
+                <div className="profile-exp bg-white p-2">
+                    <h2 className="text-primary">Education</h2>
+                </div>
+                {profile.education.length > 0 ? (
+                    <Fragment>
+                        {profile.education.map(education => (
+                            <ProfileEducation key={education._id} experience={education}/>
+                        ))}
+                    </Fragment>
+                ): (
+                    <h3>No education credentials</h3>
+                )}
             </div>
         </Fragment>
     );
