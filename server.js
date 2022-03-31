b1133729dedb25dd2ecb82d58322e85ff4c10fbb
Timmy Cho <chotimmysf@gmail.com>
2022-03-31 00:02:15 -0700

    prep for heroku

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require('path');
 
 const app = express();
 
@@ -9,13 +10,21 @@ connectDB();
 // Initialize middleware
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send('API is running.'))
-
 // Define Routes
 app.use('/API/users', require('./routes/API/users'));
 app.use('/API/auth', require('./routes/API/auth'));
 app.use('/API/posts', require('./routes/API/posts'));
 app.use('/API/profile', require('./routes/API/profile'));
+
+// Serve static assets in production
+if(process.env_NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
